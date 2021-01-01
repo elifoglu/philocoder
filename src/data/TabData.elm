405ee1bfafa41bf94174b953d62b68e84d7ccd1f
mert <elifoglumert@gmail.com>
2021-01-01 13:10:14 +0300

    remove todos and update .gitignore

--- src/data/TabData.elm
+++ src/data/TabData.elm
@@ -24,9 +24,3 @@ setContents (name, active) =
 contentBelongsToTab: String -> Content -> Bool
 contentBelongsToTab tabName content =
   member tabName content.tabs
-
-  --todo 2 - tab için sort strategy oluştur. order by date asc/desc olsun. tarihleri güncelle.
-  --todo 3 - yazılarında bold, italik kısımlar varsa onları halletmen gerek bir şekilde (beni_oku.txt'de var mesela)
-  --todo 4 - fonksiyonel programlama 101 stili yamuk yumuk oluyor, html gömebilmen gerek.
-  --todo 5 - eksik olan tek yazı fonk. programlama şimdilik
-  --todo 6 - bir view counter ekle siteye. arada bi file'a yazsın falan.
