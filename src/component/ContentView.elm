b138c4b8fb5275972db61e5bb34ba519f8603d65
mert <elifoglumert@gmail.com>
2021-01-02 12:38:43 +0300

    use markdown for nicely formatted contents

--- src/component/ContentView.elm
+++ src/component/ContentView.elm
@@ -2,6 +2,8 @@ module ContentView exposing (..)
 
 import Content exposing (..)
 import Html exposing (..)
+import Html.Attributes exposing (class, style)
+import Markdown exposing (..)
 import Msg exposing (..)
 
 
@@ -10,8 +12,8 @@ contentDiv content =
     div []
         [ h3 [] [ text content.title ]
         , text (getDateAsText content)
-        , pre []
-            [ text content.text
+        , div [ style "max-width" "600px" ]
+            [ Markdown.toHtml [] content.text
             , br [] []
             , br [] []
             ]
