4f65dbd8d82de3776dece9b18982ee406cdaa69a
mert <elifoglumert@gmail.com>
2021-05-05 14:25:38 +0300

    add initial graph

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import './style.css';
-import { Elm } from './Main.elm';
+import { Elm } from './ForceDirectedGraph.elm';
 
-let elm = Elm.Main.init({
+let elm = Elm.ForceDirectedGraph.init({
   node: document.getElementById('root')
 });
 
-elm.ports.title.subscribe( title => {
+/*elm.ports.title.subscribe( title => {
   document.title = title;
-});
+});*/
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
